d80550b6a66a4edb5f97fa810b07cd95fa66ac62
Satya Sai Vennapu <satyasaivennapu143@gmail.com>
2026-03-20 13:39:19 +0530

    added origin * to allow all

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 import router from './src/router.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
 /* ✅ FIX: define __dirname in ES module */
 const __filename = fileURLToPath(import.meta.url);
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 
 /* CORS */
 app.use(cors({
-  origin: 'http://localhost:4200',
+   origin: '*',
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type, Authorization, x-channel-token'
 }));
@@ -27,6 +27,6 @@ app.use(express.json());
 app.use('/api', router);
 
 /* Start server */
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server running on port 3000');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Server running on port ' + PORT);
 });
